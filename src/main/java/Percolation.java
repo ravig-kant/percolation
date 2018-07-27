8afd2719d9cb71efcff4cc4e5df48b308c0b001e
Ravi Gupta <36585945+ravig-kant@users.noreply.github.com>
2018-07-27 09:58:38 +0530

    first commit

---
+++ src/main/java/Percolation.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ravigu
+ */
+public class Percolation {
+
+    private final int[] openSites;
+    private final int[] sz;
+    private int numOfOpenSites = 0;
+    private final int[] connectedComponent;
+    private final int gridSize;
+    private static final boolean USE_QUICK_FIND = Boolean.valueOf(System.getProperty("useQF"));
+
+    public Percolation(int n) {
+        openSites = new int[n * n + 2];
+        sz = new int[n * n + 2];
+        gridSize = n;
+        connectedComponent = new int[n * n + 2];
+        for (int i = 0; i < n * n + 2; i++) {
+            openSites[i] = 1;
+            sz[i] = 1;
+            connectedComponent[i] = i;
+        }
+        openSites[0] = 0;
+    }
+
+    private int dimensionsToIndices(int row, int col) {
+        return (row - 1) * gridSize + col;
+        // Column is subtrated by two. 
+        //One for starting indices of array in java. Two the count begins from zero
+    }
+
+    private int indexToRow(int index) {
+        return Math.floorDiv(index - 1, gridSize);
+    }
+
+    private int indexToCol(int index) {
+        return (index % gridSize) == 0 ? gridSize : index % gridSize;
+    }
+
+    public void open(int row, int col) throws IllegalAccessException {
+        if (row > gridSize || col > gridSize || row < 1 || col < 1) {
+            throw new IllegalAccessException("Invalid inputs");
+        }
+
+        int index = dimensionsToIndices(row, col);
+        if(isOpen(index))
+            return;
+        
+        int top = (row > 1) ? dimensionsToIndices(row - 1, col) : 0;
+        int bottom = (row < gridSize) ? dimensionsToIndices(row + 1, col) : gridSize * gridSize + 1;
+        int left = (col > 1) ? dimensionsToIndices(row, col - 1) : -1;
+        int right = (col < gridSize) ? dimensionsToIndices(row, col + 1) : -1;
+
+        openSites[index] = 0;
+        //Always copy the top value to the bottom
+        //This will ensure that first index i.e. 0 will be marked for all the
+        //full sites. That makes checking wether a site is full or not a constant time operation
+
+        if (left != -1 && isOpen(left)) {
+            union(left, index);
+        }
+        if (right != -1 && isOpen(right)) {
+            union(right, index);
+        }
+        if (isOpen(top)) {
+            union(top, index);
+        }
+        if (isOpen(bottom) || bottom == gridSize * gridSize + 1) {
+//            if(USE_QUICK_FIND)
+                union(index, bottom);
+//            else
+//                union(bottom,index);
+        }
+        numOfOpenSites++;
+    }
+
+    public boolean isOpen(int index) {
+        return openSites[index] == 0;
+    }
+
+    private void union(int first, int second) {
+        if (USE_QUICK_FIND) {
+            qfUnion(first, second);
+        } else {
+            qUnion(first, second);
+        }
+    }
+
+    private void qfUnion(int first, int second) {
+        int tmp = connectedComponent[second];
+        for (int i = 1; i < gridSize * gridSize + 2; i++) {
+            if (connectedComponent[i] == tmp) {
+                connectedComponent[i] = connectedComponent[first];
+            }
+        }
+    }
+
+    private int root(int index) {
+        if (connectedComponent[index] == index) {
+            return index;
+        } else {
+            return root(connectedComponent[index]);
+        }
+    }
+
+    private void qUnion(int first, int second) {
+        int rootOfSecond = root(second);
+        int rootOfFirst = root(first);
+        
+        if(rootOfFirst==rootOfSecond)
+            return;
+        
+        if (sz[rootOfFirst] > sz[rootOfSecond]) {
+            connectedComponent[rootOfSecond] = rootOfFirst;
+            sz[rootOfFirst] += sz[rootOfSecond];
+        } else {
+            connectedComponent[rootOfFirst] = rootOfSecond;
+            sz[rootOfSecond] += sz[rootOfFirst];
+        }
+    }
+
+    public boolean isFull(int index) {
+        if (USE_QUICK_FIND) {
+            return isFullQF(index);
+        } else {
+            return isFullQU(index);
+        }
+    }
+
+    private boolean isFullQF(int index) {
+        return connectedComponent[index] == 0;
+    }
+
+    private boolean isFullQU(int index) {
+        return root(0) == root(index);
+    }
+
+    public boolean percolates() {
+        return isFull(gridSize * gridSize + 1);
+    }
+
+    public int numOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    public void printConnectedComponents() {
+        System.out.println("Connected components is");
+
+        for (int j = 1; j <= gridSize; j++) {
+            StringBuffer sb = new StringBuffer();
+            sb.append("[");
+            for (int i = ((j - 1) * gridSize) + 1; i <= j * gridSize; i++) {
+                sb.append(openSites[i] + ",");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("]");
+            sb.append("[");
+            for (int i = ((j - 1) * gridSize) + 1; i <= j * gridSize; i++) {
+                sb.append(connectedComponent[i] + ",");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("]");
+            System.out.println(sb.toString());
+        }
+    }
+
+}
---
+++ src/test/java/PercolationTest.java
@@ -0,0 +1,46 @@
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ravigu
+ */
+public class PercolationTest {
+    
+    @Test
+    public void testTwentyMetric()
+    {
+        int size = 10;
+        //System.setProperty("useQF", "true");
+        long starttm, endtm;
+        starttm = System.currentTimeMillis();
+        Percolation percolation = new Percolation(size);
+        Random randomizedRows = new Random();
+        Random randomizedCols = new Random();
+        while(!percolation.percolates()){
+            int i = randomizedRows.nextInt(size+1);
+            int j = randomizedCols.nextInt(size+1);
+            if(i==0 || j==0)
+                continue;
+            System.out.println("Punching ("+i+","+j+")");
+            try {
+                percolation.open(i, j);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(PercolationTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            percolation.printConnectedComponents();
+        }
+        endtm = System.currentTimeMillis();
+        System.out.println("total time in miliseconds " + (endtm-starttm));
+    }
+    
+}
